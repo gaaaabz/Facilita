65f54c5b54fc4d38c65f51fa55f41b96ae781180
gaaaabz <gaby8gomes@gmail.com>
2025-11-08 07:52:33 -0300

    projeto expo router base

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,24 +1,35 @@
-import { Tabs } from "expo-router";
+import { Tabs } from 'expo-router';
+import React from 'react';
 
-const TabsLayout = () => {
+import { HapticTab } from '@/components/haptic-tab';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
   return (
-    <Tabs>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}>
       <Tabs.Screen
         name="index"
         options={{
-          headerTitle: "Home",
-          title: "Home",
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
         }}
       />
       <Tabs.Screen
-        name="users/[id]"
+        name="explore"
         options={{
-          headerTitle: "User Page",
-          title: "User",
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
     </Tabs>
   );
-};
-
-export default TabsLayout;
+}
